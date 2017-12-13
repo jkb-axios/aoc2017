d7bbb78b0a0a94e7df8f89cc1a8d80aab883f435
Kipp <jkb-axios@users.noreply.github.com>
2017-12-13 01:53:00 -0500

    Update day13solution1.py

--- day13solution1.py
+++ day13solution1.py
@@ -1,9 +1,8 @@
 severity = 0
 with open('day13input.txt') as f:
   for l in f:
-    tmp = l.split(': ')
-    if len(tmp) == 2:
-      if int(tmp[0])%((int(tmp[1])-1)*2) == 0:
-        severity += int(tmp[0])*int(tmp[1])
+    idx,depth = [int(x) for x in l.split(': ')]
+    if idx%((depth-1)*2) == 0:
+      severity += idx*depth
 
 print severity
