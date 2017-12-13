1b7c40b863c73b7427629299e9674c26e587eaa9
Kipp <jkb-axios@users.noreply.github.com>
2017-12-13 01:43:48 -0500

    Update day13solution1.py

--- day13solution1.py
+++ day13solution1.py
@@ -1,14 +1,9 @@
-grid= []
+severity = 0
 with open('day13input.txt') as f:
   for l in f:
     tmp = l.split(': ')
-    if len(tmp) != 2:
-      continue
-    grid.append(int(x) for x in tmp)
-
-severity = 0
-for idx,depth in grid:
-  if idx%((depth-1)*2) == 0:
-    severity += idx*depth
+    if len(tmp) == 2:
+      if int(tmp[0])%((int(tmp[1])-1)*2) == 0:
+        severity += int(tmp[0])*int(tmp[1])
 
 print severity
